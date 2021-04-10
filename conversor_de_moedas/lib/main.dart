a695a37fc3c9872f2ab74f83235fd40ced5bdd5d
Rainer Ramos <rainermr89@hotmail.com>
2021-04-10 13:33:18 -0300

    criando função Future e corrigindo importação do async

--- conversor_de_moedas/lib/main.dart
+++ conversor_de_moedas/lib/main.dart
@@ -2,18 +2,18 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
-import 'package:async/async.dart';
+import 'dart:async';
 import 'dart:convert';
 
 const request = "https://api.hgbrasil.com/finance?format=json&key=1bd67d89";
 
 void main() async {
-
-  http.Response response = await http.get (request);
-  print(json.decode(response.body)["results"] ["currencies"]["USD"]);
-
   runApp(MaterialApp(
     home: Container()
   ));
 }
 
+Future<Map> getData() async {
+  http.Response response = await http.get (request);
+  return json.decode(response.body);
+}
