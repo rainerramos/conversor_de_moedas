9c319afd73f4bde298569412978ae695688f8333
Rainer Ramos <rainermr89@hotmail.com>
2021-04-10 13:11:08 -0300

    Json transformando uma string para um Map

--- conversor_de_moedas/lib/main.dart
+++ conversor_de_moedas/lib/main.dart
@@ -3,12 +3,14 @@ import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
 import 'package:async/async.dart';
+import 'dart:convert';
 
 const request = "https://api.hgbrasil.com/finance?format=json&key=1bd67d89";
 
 void main() async {
 
   http.Response response = await http.get (request);
+  print(json.decode(response.body));
 
   runApp(MaterialApp(
     home: Container()
