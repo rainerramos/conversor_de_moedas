d21e4bf06e421b250e72d6f91f575ea558128671
Rainer Ramos <rainermr89@hotmail.com>
2021-04-10 12:57:55 -0300

    Importando biblioteca http

--- conversor_de_moedas/lib/main.dart
+++ conversor_de_moedas/lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'package:http/http.dart' as http;
+
 const request = "https://api.hgbrasil.com/finance?format=json&key=1bd67d89";
 
 void main(){
+
+
+
   runApp(MaterialApp(
     home: Container()
   ));
 }
+
