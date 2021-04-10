fb994ee7078fa52fc09fe31adb46b9c645192e50
Rainer Ramos <rainermr89@hotmail.com>
2021-04-10 12:37:26 -0300

    recuperando dados do servidor

--- conversor_de_moedas/lib/main.dart
+++ conversor_de_moedas/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+const request = "https://api.hgbrasil.com/finance?format=json&key=1bd67d89";
+
 void main(){
   runApp(MaterialApp(
     home: Container()
