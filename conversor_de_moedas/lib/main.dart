488de9484cb56287d302ee89c8c3012127bca801
Rainer Ramos <rainermr89@hotmail.com>
2021-04-10 13:14:12 -0300

    Obtendo os dados

--- conversor_de_moedas/lib/main.dart
+++ conversor_de_moedas/lib/main.dart
@@ -10,7 +10,7 @@ const request = "https://api.hgbrasil.com/finance?format=json&key=1bd67d89";
 void main() async {
 
   http.Response response = await http.get (request);
-  print(json.decode(response.body));
+  print(json.decode(response.body)["results"] ["currencies"]["USD"]);
 
   runApp(MaterialApp(
     home: Container()
