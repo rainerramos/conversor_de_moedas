f38fa82c7bd2d156d82dd4fcbf2d13534ba79638
Rainer Ramos <rainermr89@hotmail.com>
2021-04-10 13:03:14 -0300

    Criando requisição assíncrona com espera dos dados

--- conversor_de_moedas/lib/main.dart
+++ conversor_de_moedas/lib/main.dart
@@ -2,12 +2,13 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
+import 'package:async/async.dart';
 
 const request = "https://api.hgbrasil.com/finance?format=json&key=1bd67d89";
 
-void main(){
+void main() async {
 
-
+  http.Response response = await http.get (request);
 
   runApp(MaterialApp(
     home: Container()
